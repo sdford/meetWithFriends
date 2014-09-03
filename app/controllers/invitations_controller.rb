54257f32b533602ad0fddb5652ac112eddfce9c6
Dima <dyurkans@andrew.cmu.edu>
2014-09-03 16:42:49 -0400

    Invitations created successfuly

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -10,19 +10,35 @@ class InvitationsController < ApplicationController
   end
 
   def index
+    @invitation = Invitation.all
+    #@attendee = Invitation.find_by_user_id(@invitation.user_id)
+  end
+
+  def show
     @invitation = Invitation.find(params[:id])
-    @attendee = Invitation.find_by_user_id(@invitation.user_id)
   end
   
   def create
     @invitation = Invitation.new(params[:invitation])
+    @users = User.all.map { |u| u.proper_name  }
+    @events = Event.all.map { |e| e.title  }
     if @invitation.save
       # if saved to database
       flash[:notice] = "Congratulations! You have created an invitation"
-      redirect_to @invitation
+      redirect_to 
     else
       # go back to the 'new' form
       render 'new'
+    end
+  end
+
+  def update
+    @invitation = Invitation.find(params[:id])
+    if @invitation.update_attributes(params[:invitation])
+      flash[:notice] = "Successfully updated}."
+      redirect_to home_url
+    else
+      render :action => 'edit'
     end
   end
 
